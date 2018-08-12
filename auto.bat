885aae1b3d1ca2323cd577e08aeafafef7c505cc
iamDmon <vedantkarandikar@gmail.com>
2018-08-12 19:06:29 +0530

    autoCommit

--- auto.bat
+++ auto.bat
@@ -1,6 +1,9 @@
 
 @ECHO OFF
-ECHO VEDANT
+ECHO VVVVV V    V VVVVV VVVVV
+ECHO V   V V    V   V   V   V
+ECHO VVVVV V    V   V   V   V
+ECHO V   V VVVVVV   V   VVVVV
 git add -A
 git commit -m "autoCommit"
 git push origin master
